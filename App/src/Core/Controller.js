6659ce098eb904b08b6d0df14fbfb5ca9fa8e182
Prumme <66718746+Prumme@users.noreply.github.com>
2024-05-24 16:46:42 +0200

    Revert "Feature/router backend"

--- App/src/Core/Controller.js
+++
@@ -1,4 +0,0 @@
-import {RequestHandler} from "./RequestHandler.js";
-
-export class Controller extends RequestHandler{
-}
--- App/src/server.js
+++
@@ -1,6 +0,0 @@
-import setUpApp from "./app.js";
-setUpApp().then(app => {
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000')
-    })
-})
