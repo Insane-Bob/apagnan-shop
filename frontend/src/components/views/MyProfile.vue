e7be410b879bafb974d61ac1b2a087eaf8dd5004
Prumme <aurelien23.p@gmail.com>
2024-05-27 14:42:44 +0200

    change pictures in profile

--- frontend/src/components/views/MyProfile.vue
+++ frontend/src/components/views/MyProfile.vue
@@ -7,7 +7,7 @@ import LinkCard from '@components/cards/LinkCard.vue';
     <div class="relative">
         <HeaderComponent></HeaderComponent>
             <div class="sticky top-0 z-10">
-                <img src="https://media.istockphoto.com/id/114452225/fr/photo/nain-sur-de-lherbe.jpg?s=612x612&w=0&k=20&c=uI1IjnrfEO4coxDAQALD7BLIC_EAZ88LoMtyq_8m7IM=" 
+                <img src="/src/assets/images/myProfileMenu.webp" 
                 class="w-full md:h-[60vw] lg:h-[30vw] object-cover "
                 >
                 <h1 class="text-center text-xl sm:text-xl md:text-5xl w-full tracking-widest text-white uppercase absolute z-10 bottom-1/4 -y-translate-1/2 block">Bienvenue, NOM PRENOM</h1>
@@ -18,9 +18,9 @@ import LinkCard from '@components/cards/LinkCard.vue';
             
             <div class="grid grid-cols-1 md:grid-cols-2 gap-8 justify-items-center mt-24">
             
-                <LinkCard name="Mes informations" shortDescription="Modifiez vos informations personnelles" image="/src/assets/images/green-gnome.png"></LinkCard>
+                <LinkCard name="Mes informations" shortDescription="Modifiez vos informations personnelles" image="/src/assets/images/profileMe.webp"></LinkCard>
 
-                <LinkCard name="Mes commandes" shortDescription="Consultez l'historique de vos commandes" image="/src/assets/images/green-gnome.png"></LinkCard>
+                <LinkCard name="Mes commandes" shortDescription="Consultez l'historique de vos commandes" image="/src/assets/images/commandesListe.webp"></LinkCard>
             </div>
         </main>
     </div>
