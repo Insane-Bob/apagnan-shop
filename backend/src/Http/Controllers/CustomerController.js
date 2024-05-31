e5d64ff68924555a3326d78a550f48c27f3a6644
Axel <axel77g@hotmail.com>
2024-05-31 14:53:11 +0200

    feature/policies

--- backend/src/Http/Controllers/CustomerController.js
+++ backend/src/Http/Controllers/CustomerController.js
@@ -1,5 +1,6 @@
 import { Controller } from '../../Core/Controller.js'
 import {Database} from "../../Models/index.js";
+import {CustomerPolicy} from "../Policies/CustomerPolicy.js";
 
 export class CustomerController extends Controller {
 
@@ -10,22 +11,26 @@ export class CustomerController extends Controller {
         })
     }
     show(){
+        this.can(CustomerPolicy.show, this.customer)
         this.res.json({
             customer: this.customer
         })
     }
     store(){
+        this.can(CustomerPolicy.store)
         this.res.json({
             message: 'Store'
         })
     }
     update(){
+        this.can(CustomerPolicy.update, this.customer)
         this.res.json({
             message: 'Update'
         })
     }
 
     delete(){
+        this.can(CustomerPolicy.delete, this.customer)
         this.res.json({
             message: 'Delete'
         })
