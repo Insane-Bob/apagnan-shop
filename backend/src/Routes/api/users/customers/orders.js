593a77d78e1dcfa08aed8ca4e7c147db0253e033
Axel <axel77g@hotmail.com>
2024-06-08 18:12:06 +0200

    move order

--- backend/src/Routes/api/users/customers/orders.js
+++
@@ -1,7 +0,0 @@
-import {OrderProvider} from "../../../../Http/Providers/OrderProvider.js";
-import {OrderController} from "../../../../Http/Controllers/OrderController.js";
-
-export function ordersRoutes(customerRouterGroup){
-    customerRouterGroup.group("/orders", function() {
-    }).provide(OrderProvider)
-}
