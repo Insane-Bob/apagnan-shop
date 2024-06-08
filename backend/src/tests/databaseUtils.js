c661789bd66a6c0be28c04cd408f9659f09b1018
Axel <axel77g@hotmail.com>
2024-06-08 17:06:41 +0200

    test user controller + improve utils

--- backend/src/tests/databaseUtils.js
+++ backend/src/tests/databaseUtils.js
@@ -1,46 +1,71 @@
-import {Database} from "../Models/index.js";
-import {spawnSync} from "child_process";
+import { Database } from '../Models/index.js'
+import { spawnSync } from 'child_process'
 
-
-export function useFreshDatabase(beforeAllCallback = async ()=>(null), afterAllCallback = async ()=>(null)){
-    beforeAll(async()=>{
+export function useFreshDatabase(
+    beforeAllCallback = async () => null,
+    afterAllCallback = async () => null,
+) {
+    beforeAll(async () => {
         await runMigration()
         await Database.initialize()
         await beforeAllCallback()
     }, 30000)
 
-    afterAll(async()=> {
+    afterAll(async () => {
         await afterAllCallback()
         await Database.close()
         await undoAllMigration()
     }, 30000)
 }
-export function runMigration(){
-    console.log("Running database migration")
-    spawnSync("npx", ["sequelize-cli", "db:migrate"])
+export function runMigration() {
+    console.log('Running database migration')
+    spawnSync('npx', ['sequelize-cli', 'db:migrate'])
 }
-export function undoAllMigration(){
-    console.log("Undoing all database migration")
-    spawnSync("npx", ["sequelize-cli", "db:migrate:undo:all"])
+export function undoAllMigration() {
+    console.log('Undoing all database migration')
+    spawnSync('npx', ['sequelize-cli', 'db:migrate:undo:all'])
 }
-export function mockDatabase(databaseClass){
-    if(process.env.NODE_ENV !== "test") throw new Error("Cannot mock database outside of test environment")
-    databaseClass.getInstance = jest.fn(()=>( {
-        models: {
-            User: {
-                findOne: jest.fn(),
-                create: jest.fn((obj) => obj),
-            },
-            Token: {
-                findOne: jest.fn(),
-                create: jest.fn((obj) => obj),
-            }
+
+export function getModelMock() {
+    class MockedModel {
+        constructor(obj) {
+            Object.assign(this, obj)
         }
+
+        static findOne = jest.fn(() => new MockedModel({ id: 1 }))
+        static create = jest.fn((obj) => new MockedModel(obj))
+        static destroy = jest.fn()
+        static findAll = jest.fn(() =>
+            Array(4)
+                .fill(0)
+                .map((_, i) => new MockedModel({ id: i })),
+        )
+        static findByPk = jest.fn((id) => new MockedModel({ id }))
+
+        destroy() {
+            return true
+        }
+        update(obj) {
+            Object.assign(this, obj)
+            return this
+        }
+    }
+    return MockedModel
+}
+
+export function mockDatabase(databaseClass) {
+    if (process.env.NODE_ENV !== 'test')
+        throw new Error('Cannot mock database outside of test environment')
+    databaseClass.getInstance = jest.fn(() => ({
+        models: {
+            User: getModelMock(),
+            Token: getModelMock(),
+        },
     }))
 }
 
-export async function emptyTables(){
-    for(let model in Database.getInstance().models){
-        await Database.getInstance().models[model]?.destroy({where: {}})
+export async function emptyTables() {
+    for (let model in Database.getInstance().models) {
+        await Database.getInstance().models[model]?.destroy({ where: {} })
     }
 }
