db7c108b88da0acffef0ed66f75d93e05ca193a3
Axel <axel77g@hotmail.com>
2024-06-08 18:13:44 +0200

    user basket crud

---
+++ backend/src/Models/userBasket.js
@@ -0,0 +1,40 @@
+'use strict'
+
+import { Model } from 'sequelize'
+
+export class UserBasket extends Model {
+    static associate(models) {
+        models.UserBasket.hasOne(models.User, { foreignKey: 'userId' })
+        //models.UserBasket.hasOne(models.Product, { foreignKey: 'productId' })
+    }
+
+    hasRole(role) {
+        return this.role === role
+    }
+}
+
+function model(sequelize, DataTypes) {
+    UserBasket.init(
+        {
+            productId: DataTypes.INTEGER,
+            userId: DataTypes.INTEGER,
+            quantity: DataTypes.INTEGER,
+            createdAt: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW,
+            },
+        },
+        {
+            sequelize,
+            modelName: 'UserBasket',
+        },
+    )
+
+    return UserBasket
+}
+
+export default model
