071ed51960fb0d6d61ad8e7e10781e1d939fe689
Axel <axel77g@hotmail.com>
2024-06-08 17:05:29 +0200

    clean orders

--- backend/src/Routes/api/users/customers/orders.js
+++ backend/src/Routes/api/users/customers/orders.js
@@ -3,10 +3,5 @@ import {OrderController} from "../../../../Http/Controllers/OrderController.js";
 
 export function ordersRoutes(customerRouterGroup){
     customerRouterGroup.group("/orders", function() {
-        this.get('/:order', OrderController, 'show')
-        this.get('/', OrderController, 'index')
-        this.post('/', OrderController, 'store')
-        this.put('/:order', OrderController, 'update')
-        this.delete('/:order', OrderController, 'destroy')
     }).provide(OrderProvider)
 }
