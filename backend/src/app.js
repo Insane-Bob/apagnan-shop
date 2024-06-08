52edd9687b9a177f9a2cae303a5ebbb3247aa121
Axel <axel77g@hotmail.com>
2024-06-08 17:03:07 +0200

    handle not implemented routs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,25 @@
-import dotenv from "dotenv";
-import path from "path";
+import dotenv from 'dotenv'
+import path from 'path'
 dotenv.config({
-    path: path.resolve(".env")
-});
-import express from "express";
-import {AuthMiddleware} from "./Http/Middlewares/AuthMiddleware.js";
-import {Router} from "./Core/Router.js";
+    path: path.resolve('.env'),
+})
+import express from 'express'
+import { AuthMiddleware } from './Http/Middlewares/AuthMiddleware.js'
+import { Router } from './Core/Router.js'
 
-async function setUpApp(){
+async function setUpApp() {
     console.time('Server started in')
+
     const app = express()
-    const router = new Router(app)
     app.use(express.json())
-    router
-        .middleware(AuthMiddleware)
-        .init(app)
+
+    const router = new Router(app)
+    await router.middleware(AuthMiddleware).init(app)
+
+    app.all('*', (req, res) => {
+        res.status(405).json({ code: 405, message: 'not implemented' })
+    })
+
     console.timeEnd('Server started in')
     return app
 }
