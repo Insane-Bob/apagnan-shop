98b3c9d752617f8639f1992bf1ccbb2ec02ee89b
Axel <axel77g@hotmail.com>
2024-06-08 18:11:58 +0200

    delete customer routes and controller

--- backend/src/Http/Controllers/CustomerController.js
+++
@@ -1,5 +0,0 @@
-import { Controller } from '../../Core/Controller.js'
-import { Database } from '../../Models/index.js'
-import { CustomerPolicy } from '../Policies/CustomerPolicy.js'
-
-export class CustomerController extends Controller {}
