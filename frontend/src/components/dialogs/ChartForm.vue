63685ead201d0ba9a9c837517182dde05f8b3c38
Prumme <aurelien23.p@gmail.com>
2024-06-12 17:31:48 +0200

    start of dialog dashboard

---
+++ frontend/src/components/dialogs/ChartForm.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="max-h-[70vh] h-[70vh] w-[60vw] max-w-[60vw]">
+        <h1 class="text-2xl tracking-wider">Ajouter un graphique</h1>
+        <form>
+
+        </form>
+    </div>
+</template>
