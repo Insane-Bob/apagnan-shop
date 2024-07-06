09c18d1ebd459579f67718e71ab975f77199eef5
Prumme <aurelien23.p@gmail.com>
2024-07-06 22:28:49 +0200

    admin router guard

---
+++ frontend/src/components/Drawers/CartDrawer.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import {
+SheetContent,
+SheetHeader,
+SheetTitle
+} from '@/components/ui/sheet';
+
+import CartCard from '@components/cards/CartCard.vue';
+import Button from '@components/ui/button/Button.vue';
+import { onMounted } from 'vue';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+console.log(localStorage)
+const userId = JSON.parse(localStorage.getItem('user') as string).user.id
+const cartUrl = API_BASE_URL + '/users/' + userId + '/basket'
+
+
+onMounted(() => {
+    fetchCart()
+})
+
+const fetchCart = async () => {
+    const response = await fetch(cartUrl)
+    const data = await response.json()
+    console.log(data)
+    data.product.forEach((c: any) => {
+        console.log(c)
+    })
+}
+
+
+const products = [
+    {
+        id: 1,
+        name: 'product-1',
+        price: 10,
+        quantity: 1,
+        imgPath: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 2,
+        name: 'product-2',
+        price: 20,
+        quantity: 2,
+        imgPath: 'https://via.placeholder.com/150'
+    },
+    {
+        id: 3,
+        name: 'product-3',
+        price: 30,
+        quantity: 3,
+        imgPath: 'https://via.placeholder.com/150'
+    }
+]
+</script>
+
+<template>
+    <SheetContent class="flex flex-col">
+    <SheetHeader>
+        <SheetTitle class="uppercase tracking-wider">Ajouté au panier</SheetTitle>
+    </SheetHeader>
+    <div  class="w-full max-h-[80vh] overflow-y-scroll grow flex flex-col justify-start divide-y divide-primary">
+        <div v-for="product in products" :key="product.id" class="flex flex-col justify-start">
+            <CartCard :product="product" />
+        </div>
+    </div>
+    <Button class="w-full uppercase tracking-wider font-light">Règlement</Button>
+    </SheetContent>
+</template>
--- frontend/src/routes/admin.ts
+++ frontend/src/routes/admin.ts
@@ -3,12 +3,49 @@ import AdminCustomers from '@/components/views/admin/AdminCustomers.vue'
 import AdminCollections from '@/components/views/admin/collections/AdminCollections.vue'
 import AdminUsers from '@/components/views/admin/users/AdminUsers.vue'
 import WorkInProgress from '@components/views/WorkInProgress/WorkInProgress.vue'
+import { apiClient } from '@/lib/apiClient'
+import { useToast } from '@/components/ui/toast/use-toast'
 
 export const adminRoutes = [
     {
         path: '/admin',
         component: AdminLayout,
         redirect: '/admin/dashboard',
+        beforeEnter: (to: any, from: any) => {
+            // Check if user is authenticated && is admin
+            // If not, redirect to login page
+            if (localStorage.getItem('accessToken')) {
+                const { toast } = useToast()
+                const result = apiClient
+                    .get('me')
+                    .then((response) => {
+                        console.log(response.data)
+                        if (response.data.user.role === 'admin') {
+                            return true
+                        } else {
+                            toast({
+                                title: 'Erreur',
+                                description:
+                                    "Vous n'êtes pas autorisé à accéder à cette page.",
+                                variant: 'destructive',
+                            })
+                            return false
+                        }
+                    })
+                    .catch((error) => {
+                        toast({
+                            title: 'Erreur',
+                            description:
+                                error.response.data.message ||
+                                "Vous n'êtes pas autorisé à accéder à cette page.",
+                            variant: 'destructive',
+                        })
+                        return false
+                    })
+                return result
+            }
+            return false
+        },
         children: [
             {
                 path: 'dashboard',
