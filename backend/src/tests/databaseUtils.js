c75cac2d2aef1ea86d5c125c2e19ac8adccbce6d
Prumme <aurelien23.p@gmail.com>
2024-07-13 21:50:42 +0200

    add address mock

--- backend/src/tests/databaseUtils.js
+++ backend/src/tests/databaseUtils.js
@@ -76,6 +76,7 @@ export function mockDatabase(databaseClass) {
             UserBasket: getModelMock(),
             Collection: getModelMock(),
             Upload: getModelMock(),
+            Address: getModelMock(),
         },
     }))
 }
