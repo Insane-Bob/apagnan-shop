2c85628ed4b08ca13988725c1a29090d8b4204d4
Axel <axel77g@hotmail.com>
2024-07-13 11:39:10 +0200

    update user widget payload

--- backend/src/Validator/UserWidgetValidator.js
+++ backend/src/Validator/UserWidgetValidator.js
@@ -1,21 +1,25 @@
 import { Validator } from './Validator.js'
 import { z } from 'zod'
 
-export class UserWidgetValidator extends Validator{
-  constructor() {
-    super(UserWidgetValidator.put())
-  }
+export class UserWidgetValidator extends Validator {
+    constructor() {
+        super(UserWidgetValidator.put())
+    }
 
-  static put(){
-    let jsonSchema = z.object({
-      name: z.string(),
-      styles: z.object({
-        grid: z.string().optional(),
-      })
-    })
+    static put() {
+        let jsonSchema = z.object({
+            id: z.string(),
+            active: z.boolean(),
+            gs: z.object({
+                x: z.number(),
+                y: z.number(),
+                width: z.number(),
+                height: z.number().optional(),
+            }),
+        })
 
-    return z.object({
-      data: z.array(jsonSchema)
-    })
-  }
+        return z.object({
+            data: z.array(jsonSchema),
+        })
+    }
 }
