f69f98d7b25b2409d88117bfe013ad38bc7783e6
Prumme <aurelien23.p@gmail.com>
2024-07-20 14:30:14 +0200

    remove admin right

--- backend/src/Http/Policies/UserWidgetPolicy.js
+++ backend/src/Http/Policies/UserWidgetPolicy.js
@@ -1,10 +1,5 @@
-import { USER_ROLES } from '../../Models/SQL/user.js'
-
 export class UserWidgetPolicy {
     static show(user, userResource) {
-        if (user.hasRole(USER_ROLES.ADMIN)) {
-            return true
-        }
         return user.id === userResource.id
     }
 }
