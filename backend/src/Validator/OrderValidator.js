2d1dc7ece605e2c5561d8bb3b34636b11d6fdf21
Prumme <aurelien23.p@gmail.com>
2024-07-20 11:23:23 +0200

    fix orderValidator

--- backend/src/Validator/OrderValidator.js
+++ backend/src/Validator/OrderValidator.js
@@ -46,11 +46,12 @@ export class OrderValidator extends Validator {
                 req.query.get('withProducts') === 'true',
             )
 
-        if (req.query.has('customerId')) {
-            req.query.set(
-                'customerId',
-                req.query.get('customerId').split(',').map(Number),
-            )
+            if (req.query.has('customerId')) {
+                req.query.set(
+                    'customerId',
+                    req.query.get('customerId').split(',').map(Number),
+                )
+            }
         }
     }
 
