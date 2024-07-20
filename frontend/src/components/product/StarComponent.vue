fab21254b3b1ed7f9976f8f0e92934247b8e1cf1
Prumme <aurelien23.p@gmail.com>
2024-07-20 13:31:50 +0200

    better admin Reviews dashboard

---
+++ frontend/src/components/product/StarComponent.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+defineProps<{
+    value: number
+}>()
+
+</script>
+
+<template>
+    <div>
+        <div class="flex gap-1">
+            <ion-icon
+                v-for="index in 5"
+                :key="index"
+                :name="index <= value ? 'star' : 'star-outline'"
+                class="text-primary"
+            ></ion-icon>
+        </div>
+    </div>
+</template>
--- frontend/src/types/TableColumns.ts
+++ frontend/src/types/TableColumns.ts
@@ -2,6 +2,7 @@ export interface TableColumns {
     label: string
     key: string
     sorting?: boolean
+    maxWidth: string
     position?: 'left' | 'center' | 'right'
     toDisplay?: (value: any) => string
     sortingType?: 'string' | 'number' | 'date' | 'boolean' | 'custom' | 'none'
