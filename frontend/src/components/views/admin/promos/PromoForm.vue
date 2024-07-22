a3761154a3a8ca9fc9c08ac0bc96bf2892a37936
Ilyam Dupuis <ilyamdupuis0903@gmail.com>
2024-07-22 11:40:26 +0200

    fix(validator): add translation and display errors for promo form

--- frontend/src/components/views/admin/promos/PromoForm.vue
+++ frontend/src/components/views/admin/promos/PromoForm.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import FormError from '@/components/Forms/FormError.vue'
 import FormGrid from '@/components/Forms/FormGrid.vue'
 import FormInput from '@/components/Inputs/FormInput.vue'
 import Button from '@/components/ui/button/Button.vue'
@@ -6,7 +7,7 @@ import {
     DialogClose,
     DialogFooter,
     DialogHeader,
-    DialogTitle
+    DialogTitle,
 } from '@/components/ui/dialog'
 import { Label } from '@/components/ui/label'
 import {
@@ -15,73 +16,81 @@ import {
     SelectGroup,
     SelectItem,
     SelectTrigger,
-    SelectValue
+    SelectValue,
 } from '@/components/ui/select'
 import { Switch } from '@/components/ui/switch'
 import { ApiClient } from '@/lib/apiClient'
 import { Promo } from '@types'
-import { defineProps, ref } from 'vue'
+import { ref } from 'vue'
+import { useToast } from '@/components/ui/toast/use-toast'
 
 const apiClient = new ApiClient()
 
-const props = defineProps<{
-  promo?: Promo,
-}>()
-
 const emit = defineEmits(['reload-promo'])
 
+const { toast } = useToast()
 
-const code = ref(props.promo?.code || '')
-const value = ref(props.promo?.value || '')
-const type = ref(props.promo?.type || '')
-const promoted = ref(props.promo?.promoted || false)
-const available = ref(props.promo?.available || false)
+const code = ref('')
+const value = ref('')
+const type = ref('')
+const promoted = ref(false)
+const available = ref(false)
+
+const errors = ref(null)
 
 const onSubmit = () => {
-    if (props.promo) {
-        // updatePromo()
-    } else {
-        createPromo()
-    }
+    createPromo()
 }
-
 const createPromo = async () => {
+    try {
+        let body: Promo = {
+            code: code.value.toUpperCase(),
+            value: value.value,
+            type: type.value,
+            promoted: promoted.value,
+            available: available.value,
+        }
 
-    let body: Promo  = {
-        code: code.value.toUpperCase(),
-        value: value.value,
-        type: type.value,
-        promoted: promoted.value,
-        available: available.value,
-    }
+        const response = await apiClient.post('promos/', body)
 
-    const response = await apiClient.post('promos/', body)
-
-    if (response.status === 201) {
-        emit('reload-promo')
+        if (response.status === 201) {
+            toast({
+                title: 'Succés',
+                description: 'Votre code promo a été crée',
+            })
+            emit('reload-promo')
+        }
+    } catch (error) {
+        errors.value = error.response.data.errors
     }
 }
-
-
-
 </script>
 
 <template>
     <form @submit.prevent="onSubmit">
         <DialogHeader>
-          <DialogTitle>{{ props.promo? 'Modifier la promotion': 'Créer une promotion'}}</DialogTitle>
+            <DialogTitle>Créer une promotion</DialogTitle>
         </DialogHeader>
 
         <FormGrid>
-
-            <FormInput class="col-start-1 col-span-full" required>
-                <template #label>Code</template>
+            <FormInput
+                name="code"
+                :errors="errors"
+                class="col-start-1 col-span-full"
+                required
+            >
+                <template #label>Nom du code</template>
                 <template #input="inputProps">
                     <input type="text" v-model="code" v-bind="inputProps" />
                 </template>
             </FormInput>
 
-            <FormInput class="col-start-1 col-span-6" required>
+            <FormInput
+                name="value"
+                :errors="errors"
+                class="col-start-1 col-span-6"
+                required
+            >
                 <template #label>Valeur</template>
                 <template #input="inputProps">
                     <input type="number" v-model="value" v-bind="inputProps" />
@@ -90,50 +99,66 @@ const createPromo = async () => {
 
             <Select v-model="type">
                 <SelectTrigger class="col-span-6 self-end h-11">
-                <SelectValue placeholder="Selectionner un type" />
+                    <SelectValue placeholder="Selectionner un type" />
                 </SelectTrigger>
                 <SelectContent>
-                <SelectGroup>
-                    <SelectItem value="percent">
-                    Pourcentage
-                    </SelectItem>
-                    <SelectItem value="amount">
-                    Fixe
-                    </SelectItem>
-                </SelectGroup>
+                    <SelectGroup>
+                        <SelectItem value="percent"> Pourcentage </SelectItem>
+                        <SelectItem value="amount"> Fixe </SelectItem>
+                    </SelectGroup>
                 </SelectContent>
+                <FormError> </FormError>
             </Select>
 
-
-            <div class="row-span-1 col-start-1 col-span-full flex items-center gap-x-4">
+            <div
+                class="row-span-1 col-start-1 col-span-full flex items-center gap-x-4"
+            >
                 <Label
-                @click="available = !available"
-                for="available"
-                class=" h-4 block text-xs text-zinc-800 font-medium first-letter:uppercase whitespace-nowrap">
+                    @click="available = !available"
+                    for="available"
+                    class="h-4 block text-xs text-zinc-800 font-medium first-letter:uppercase whitespace-nowrap"
+                >
                     Disponible
                 </Label>
-                <Switch name="available" type="text" :checked="available" @click="available = !available" class=""/>
+                <Switch
+                    name="available"
+                    type="text"
+                    :checked="available"
+                    @click="available = !available"
+                    class=""
+                />
             </div>
 
-            <div class="row-span-1 col-start-1 col-span-full flex items-center gap-x-4">
+            <div
+                class="row-span-1 col-start-1 col-span-full flex items-center gap-x-4"
+            >
                 <Label
-                @click="promoted = !promoted"
-                for="promoted"
-                class=" h-4 block text-xs text-zinc-800 font-medium first-letter:uppercase whitespace-nowrap">
+                    @click="promoted = !promoted"
+                    for="promoted"
+                    class="h-4 block text-xs text-zinc-800 font-medium first-letter:uppercase whitespace-nowrap"
+                >
                     Mise en avant
                 </Label>
-                <Switch name="promoted" type="text" :checked="promoted" @click="promoted = !promoted" class=""/>
+                <Switch
+                    name="promoted"
+                    type="text"
+                    :checked="promoted"
+                    @click="promoted = !promoted"
+                    class=""
+                />
             </div>
-
         </FormGrid>
 
         <DialogFooter>
             <DialogClose>
-                <Button type="button" variant="destructive" class="border-slate-300">Fermer</Button>
+                <Button
+                    type="button"
+                    variant="destructive"
+                    class="border-slate-300"
+                    >Fermer</Button
+                >
             </DialogClose>
-            <DialogClose>
-                <Button>{{ props.promo?'Modifier': 'Créer' }}</Button>
-            </DialogClose>
+            <DialogClose> <Button>Créer</Button> </DialogClose>
         </DialogFooter>
     </form>
 </template>
