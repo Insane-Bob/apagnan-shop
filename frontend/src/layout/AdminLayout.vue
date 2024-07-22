03cf86c312f71a6645209e19f6d9e2206661bbeb
Prumme <aurelien23.p@gmail.com>
2024-07-22 10:27:52 +0200

    :pen: translate dashboard breadcrumb

--- frontend/src/layout/AdminLayout.vue
+++ frontend/src/layout/AdminLayout.vue
@@ -5,6 +5,7 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import Search from '@components/Dashboard/Search.vue'
 import UserNav from '@components/Dashboard/UserNav.vue'
+import {translate } from '@utils/translateBreadcrumb.js'
 
 const routes = computed(() => {
     return adminRoutes[0].children.map((route) => {
@@ -24,8 +25,9 @@ const links = computed(() => {
         .split('/')
         .slice(1)
         .map((path, index) => {
+          console.log(path)
             return [
-                path,
+                translate[path] || path,
                 $route.path
                     .split('/')
                     .slice(0, index + 2)
