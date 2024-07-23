c0a0114728896707bd8c9980cef45cab2c9e420a
Prumme <66718746+Prumme@users.noreply.github.com>
2024-07-23 16:40:14 +0200

    Update frontend/src/components/paginations/SimplePagination.vue

--- frontend/src/components/paginations/SimplePagination.vue
+++ frontend/src/components/paginations/SimplePagination.vue
@@ -89,33 +89,5 @@ const pagination = computed(() => {
             <ion-icon name="chevron-forward-outline" />
             <ion-icon name="chevron-forward-outline" />
         </Button>
-        <!--        <button-->
-        <!--            @click="pagination.prevPage"-->
-        <!--            :class="{-->
-        <!--                'text-gray-300 cursor-not-allowed':-->
-        <!--                    pagination.currentPage === 1,-->
-        <!--            }"-->
-        <!--        >-->
-        <!--            <ion-icon name="chevron-back-outline"></ion-icon>-->
-        <!--        </button>-->
-        <!--        <button v-for="i in [2,1]" :key="i">-->
-        <!--            <span v-if="(pagination.currentPage - i) <= pagination.total && pagination.currentPage - i >= 1" @click="pagination.goToPage(pagination.currentPage - i)">{{ pagination.currentPage - i }}</span>-->
-        <!--        </button>-->
-        <!--        <button class="bg-primary text-white h-6 w-6 rounded-sm">-->
-        <!--            {{ pagination.currentPage }}-->
-        <!--        </button>-->
-        <!--        <button v-for="i in 2" :key="i">-->
-        <!--            <span v-if="(pagination.currentPage + i) <= pagination.total" @click="pagination.goToPage(pagination.currentPage + i)">{{ pagination.currentPage + i }}</span>-->
-        <!--        </button>-->
-        <!--        <button-->
-        <!--            @click="pagination.nextPage"-->
-        <!--            :class="{-->
-        <!--                'text-gray-300 cursor-not-allowed':-->
-        <!--                    pagination.currentPage >=-->
-        <!--                    pagination.total,-->
-        <!--            }"-->
-        <!--        >-->
-        <!--            <ion-icon name="chevron-forward-outline"></ion-icon>-->
-        <!--        </button>-->
     </div>
 </template>
